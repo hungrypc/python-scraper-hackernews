f8d3713f6ef73d35b3f04ace031d5595397f882d
hpc <philipchanhk626@gmail.com>
2020-06-28 10:41:35 -0400

    starting to scrape hackernews title, href, and points

--- scrape.py
+++ scrape.py
@@ -4,9 +4,22 @@ from bs4 import BeautifulSoup
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
-links = soup.select('.storylink')
-votes = soup.select('.score')
+hn_links = soup.select('.storylink')
+hn_votes = soup.select('.score')
 
+
+def create_custom_hn(links, votes):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        points = int(votes[index].getText().replace(' points', ''))
+        print(points)
+        hn.append({'title': title, 'link': href})
+    return hn
+
+
+create_custom_hn(hn_links, hn_votes)
 
 # print(soup.body)  # returns the body of the page
 # print(soup.body.contents)  # returns contents in body as list
