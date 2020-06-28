7a76ccce2bc879f98ad747385cb55afc9a879072
hpc <philipchanhk626@gmail.com>
2020-06-28 10:49:32 -0400

    appending if points > 99

--- scrape.py
+++ scrape.py
@@ -1,25 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
 hn_links = soup.select('.storylink')
-hn_votes = soup.select('.score')
+hn_subtext = soup.select('.subtext')
 
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for index, item in enumerate(links):
-        title = links[index].getText()
-        href = links[index].get('href', None)
-        points = int(votes[index].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
 
-create_custom_hn(hn_links, hn_votes)
+data = create_custom_hn(hn_links, hn_subtext)
+pprint.pprint(data)
 
 # print(soup.body)  # returns the body of the page
 # print(soup.body.contents)  # returns contents in body as list
