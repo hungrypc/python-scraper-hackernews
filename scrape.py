8126088d7d5909f51f8d587f981457d25015f48d
hpc <philipchanhk626@gmail.com>
2020-06-28 10:32:12 -0400

    playing with soup select

--- scrape.py
+++ scrape.py
@@ -2,5 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(res.text, 'html.parser')
 
-print(res.text)
+links = soup.select('.storylink')
+votes = soup.select('.score')
+
+
+# print(soup.body)  # returns the body of the page
+# print(soup.body.contents)  # returns contents in body as list
+
+# print(soup.find_all('div'))  # retuns all the divs as a list
+# print(soup.find(id="score_20514755"))  # find element by id
